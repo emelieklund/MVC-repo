458c04a80f0a000df9c686b345b0802e3acdece6
emelieklund <emelieeeklund@gmail.com>
2024-05-25 11:02:34 +0200

    Created unit tests for GameBoard class

---
+++ tests/Card/GameBoardTest.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Card;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class DeckOfCards.
+ */
+class GameBoardTest extends TestCase
+{
+    private $drawHand = [
+        "5_of_spades",
+        "3_of_spades",
+        "9_of_hearts",
+        "6_of_hearts",
+        "4_of_clubs",
+        "5_of_hearts",
+        "4_of_diamonds",
+        "5_of_diamonds",
+        "9_of_diamonds",
+        "7_of_clubs",
+        "2_of_diamonds",
+        "10_of_clubs",
+        "2_of_hearts",
+        "Q_of_clubs",
+        "4_of_hearts",
+        "4_of_spades",
+        "3_of_diamonds",
+        "6_of_diamonds",
+        "J_of_clubs",
+        "A_of_diamonds",
+        "J_of_diamonds",
+        "A_of_clubs",
+        "2_of_spades",
+        "5_of_clubs",
+        "10_of_hearts"
+    ];
+
+    /**
+     * Tests getObject() method for GameBoard.
+     */
+    public function testGameBoardGetObjects(): void
+    {
+        $gameBoard = new GameBoard();
+
+        $objects = $gameBoard->getObjects();
+
+        for ($i = 0; $i < 25; $i++) {
+            $objects[$i]->setHolderCard($this->drawHand[$i]);
+        }
+
+        $card = $objects[2]->getHolderCard();
+
+        $this->assertEquals("9_of_hearts", $card);
+    }
+
+    /**
+     * Tests getHolderIds() method for GameBoard.
+     */
+    public function testGameBoardGetHolderIds(): void
+    {
+        $gameBoard = new GameBoard();
+
+        $holderIds = $gameBoard->getHolderIds();
+
+        $this->assertEquals($holderIds[4], 5);
+    }
+
+    /**
+     * Tests getHolderCards() method for GameBoard.
+     */
+    public function testGameBoardGetHolderCards(): void
+    {
+        $gameBoard = new GameBoard();
+
+        $objects = $gameBoard->getObjects();
+
+        for ($i = 0; $i < 25; $i++) {
+            $objects[$i]->setHolderCard($this->drawHand[$i]);
+        }
+
+        $holderCards = $gameBoard->getHolderCards();
+
+        $this->assertEquals($holderCards[24], "10_of_hearts");
+    }
+
+    /**
+     * Tests getIdAndCard() method for GameBoard.
+     */
+    public function testGameBoardGetIdAndCard(): void
+    {
+        $gameBoard = new GameBoard();
+
+        $objects = $gameBoard->getObjects();
+
+        for ($i = 0; $i < 25; $i++) {
+            $objects[$i]->setHolderCard($this->drawHand[$i]);
+        }
+
+        $idAndCard = $gameBoard->getIdAndCard();
+
+        $this->assertEquals($idAndCard[9], [10, "7_of_clubs"]);
+    }
+
+    /**
+     * Tests columns() method for GameBoard.
+     */
+    public function testGameBoardColumns(): void
+    {
+        $gameBoard = new GameBoard();
+
+        $columns = $gameBoard->columns();
+
+        $this->assertEquals(count($columns), 5);
+        $this->assertEquals($columns[1][1][0], 7);
+    }
+
+    /**
+     * Tests rows() method for GameBoard.
+     */
+    public function testGameBoardRows(): void
+    {
+        $gameBoard = new GameBoard();
+
+        $objects = $gameBoard->getObjects();
+
+        for ($i = 0; $i < 25; $i++) {
+            $objects[$i]->setHolderCard($this->drawHand[$i]);
+        }
+
+        $rows = $gameBoard->rows();
+
+        $this->assertEquals(count($rows), 5);
+        $this->assertEquals($rows[2][2][1], "2_of_hearts");
+    }
+
+    /**
+     * Tests ifFull() method for GameBoard when full.
+     */
+    public function testGameBoardIfFullTrue(): void
+    {
+        $gameBoard = new GameBoard();
+
+        $objects = $gameBoard->getObjects();
+
+        for ($i = 0; $i < 25; $i++) {
+            $objects[$i]->setHolderCard($this->drawHand[$i]);
+        }
+
+        $rows = $gameBoard->rows();
+
+        $this->assertTrue($gameBoard->ifFull($rows[1]));
+    }
+
+
+    /**
+     * Tests ifFull() method for GameBoard when not full.
+     */
+    public function testGameBoardIfFullFalse(): void
+    {
+        $gameBoard = new GameBoard();
+
+        $rows = $gameBoard->rows();
+
+        $this->assertFalse($gameBoard->ifFull($rows[1]));
+    }
+}
